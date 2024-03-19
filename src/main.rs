d575bc9ee9c8bd5e9c2d4a5cd316b7999004bf94
NandorKovacs <nandor1kovacs@gmail.com>
2024-03-19 22:59:54 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::fs;
+use std::fmt::{format, write};
+use std::fs::{self, create_dir, rename};
 use std::io::ErrorKind;
 use std::path::Path;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
     map: HashMap<String, String>,
+}
+
+fn read_config(config_path: &Path) -> Config {
+    match fs::read_to_string(&config_path) {
+        Ok(config_string) => {
+            let c: Config = toml::from_str(&config_string).unwrap();
+            c
+        }
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => {
+                let config = Config {
+                    map: HashMap::new(),
+                };
+                let _ = fs::write(&config_path, toml::to_string(&config).unwrap());
+                config
+            }
+            _ => {
+                panic!();
+            }
+        },
+    }
 }
 
 fn nginx_proxy_build(from: &str, to: &str) -> String {
@@ -25,30 +47,34 @@ fn nginx_proxy_build(from: &str, to: &str) -> String {
     )
 }
 
+// fn request_cert(hostname: &str) {}
+
+// fn certificates(config: &Config) {}
+
+fn generate_configs(homepath: &Path, config: &Config) {
+    let nginx_folder = homepath.join("nginx");
+    let old = homepath.join("nginx-old");
+
+    if let Err(e) = rename(&nginx_folder, &old) {
+        if e.kind().ne(&ErrorKind::NotFound) {
+            panic!("couldn't move nginx folder to nginx-old {}", e);
+        }
+    }
+
+    create_dir(&nginx_folder).unwrap();
+
+    for (source, target) in &config.map {
+        fs::write(nginx_folder.join(format!("{}.nginx", source)), nginx_proxy_build(&source, &target)).unwrap();
+    }
+}
+
 fn main() {
     let homepath: &Path = Path::new(".");
     let config_path = homepath.join("config.toml");
-    let nginx_folder = homepath.join("nginx");
 
-    let config: Config = match fs::read_to_string(&config_path) {
-        Ok(config_string) => {
-            let c: Config = toml::from_str(&config_string).unwrap();
-            c
-        }
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => Config {
-                map: HashMap::new(),
-            },
-            _ => {
-                panic!();
-            }
-        },
-    };
+    let config = read_config(&config_path);
 
-    for (key, value) in &config.map {
-        println!("{}, {}", key, value);
-        let _ = fs::write(nginx_folder.join(key), nginx_proxy_build(key, value));
-    }
-
-    let _ = fs::write(&config_path, toml::to_string(&config).unwrap());
+    generate_configs(&homepath,&config);
+    // certificates(&config);
+    
 }
