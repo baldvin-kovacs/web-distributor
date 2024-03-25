1d85420944dd32c1019aa8fd41670045b49904a5
NandorKovacs <nandor1kovacs@gmail.com>
2024-03-25 02:28:17 +0100

    cli with clap, login management with login groups, route management through cli

--- README.md
+++ README.md
@@ -12,13 +12,13 @@ When you run it the first time, it generates a home directory in /etc/web-distri
 To configure it, edit /etc/web-distributor/config.toml. You can edit following variables:
 - home: this defines where to put the webserver configuration files. defaults to /etc/web-distributor.
 - acme_redirect_configs: where to put the acme-redirect configuration files. Defaults to /etc/acme-redirect.d.
-- \[map\]: Here, you can put a list of your desired reverse proxies, separated by a linebreak. "example.com" = "127.0.0.1" would be a correct syntax.
+- \[routes\]: Here, you can put a list of your desired reverse proxies, separated by a linebreak. "example.com" = "127.0.0.1" would be a correct syntax.
+- \[login_groups\] Here, you can specify which domain should belong to which login group. For each login groups there will be a .htaccess file generated. Using the `web-distributor login-group` subcommand, logins (name, password pairs) can be added to login groups.
 
-You can also specify a different path for the configuration file like this: `web-distributor <config-file>`.
+It is not needed to edit the config file manually now. It stays supported, however now there is the option to use the different subcommands to manage routres and logins. Run `web-distributor --help` for more documentation.
+
+You can also specify a different path for the configuration file like this: `web-distributor -c config-file`.
 
 If you leave the defaults like this, you will have to manually include the configuration files to nginx. To do this, add the line `include /etc/web-distributor/nginx/*` to your nginx.conf.
 
 Every time you run this script with a new configuration, run `acme-redirect renew` afterwards. Depending on how you configured acme-redirect, also run `systemctl reload nginx`.
-
-# TODO
-- optional password authentication in config for some domains
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,95 @@
+use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::{self, create_dir_all, read_dir, rename};
 use std::io::ErrorKind;
 use std::path::Path;
+use std::process::exit;
 use std::time::SystemTime;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    #[arg(short, long)]
+    config: Option<String>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Generate {},
+    Add {
+        domain: String,
+        target: String,
+
+        #[arg(short, long)]
+        force: bool,
+    },
+    Remove {
+        domain: String,
+    },
+    List {},
+    LoginGroup {
+        #[command(subcommand)]
+        pwcommands: PasswordCommands,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+enum PasswordCommands {
+    Create {
+        login_group: String,
+    },
+    Remove {
+        login_group: String,
+    },
+    List {},
+    Apply {
+        domain: String,
+        login_group: String,
+
+        #[arg(short, long)]
+        force: bool,
+    },
+    Disable {
+        domain: String,
+    },
+    AddLogin {
+        login_group: String,
+        name: String,
+        password: Option<String>,
+
+        #[arg(short, long)]
+        force: bool,
+    },
+    RevokeLogin {
+        login_group: String,
+        name: String,
+    },
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
     home: String,
     acme_redirect_configs: String,
-    map: HashMap<String, String>,
+    routes: HashMap<String, String>,
+    login_groups: HashMap<String, String>,
 }
 
 fn read_config(config_path: &Path) -> Config {
     match fs::read_to_string(&config_path) {
-        Ok(config_string) => {
-            let c: Config = toml::from_str(&config_string).unwrap();
-            c
-        }
+        Ok(config_string) => toml::from_str(&config_string).unwrap(),
         Err(e) => match e.kind() {
             ErrorKind::NotFound => {
                 let config = Config {
                     home: "/etc/web-distributor".to_string(),
                     acme_redirect_configs: "/etc/acme-redirect.d".to_string(),
-                    map: HashMap::new(),
+                    routes: HashMap::new(),
+                    login_groups: HashMap::new(),
                 };
-                let _ = fs::write(&config_path, toml::to_string(&config).unwrap());
+                write_config(&config, &config_path);
                 config
             }
             _ => {
@@ -35,59 +99,29 @@ fn read_config(config_path: &Path) -> Config {
     }
 }
 
-fn nginx_proxy_build(from: &str, to: &str) -> String {
-    format!(
-"server {{
-        listen 443 ssl http2;
-        listen [::]:443 ssl http2;
-
-        server_name {from};
-
-        ssl_certificate /var/lib/acme-redirect/live/{from}/fullchain;
-        ssl_certificate_key /var/lib/acme-redirect/live/{from}/privkey;
-        ssl_session_timeout 1d;
-        ssl_session_cache shared:MozSSL:10m;  # about 40000 sessions
-        ssl_session_tickets off;
-        
-        ssl_protocols TLSv1.2 TLSv1.3;
-        ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
-        ssl_prefer_server_ciphers off;
-
-        add_header Strict-Transport-Security \"max-age=63072000\" always;
-
-        ssl_stapling on;
-        ssl_stapling_verify on;
-        ssl_trusted_certificate /var/lib/acme-redirect/live/{from}/chain;
-        resolver 127.0.0.1;
-
-
-        location / {{
-                proxy_set_header Host $http_host;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Scheme $scheme;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade $http_upgrade;
-                proxy_set_header Connection \"Upgrade\";
-                client_max_body_size 0;
-
-                proxy_pass http://{to}/;
-        }}
-}}")
+fn nginx_proxy_build(from: &str, to: &str, login_group_path: Option<String>) -> String {
+    match login_group_path {
+        Some(lg) => format!(
+            include_str!("nginx.conf"),
+            from = from,
+            to = to,
+            login_group = format!(
+                "auth_basic \"login\";
+        auth_basic_user_file {};",
+                lg
+            )
+        ),
+        None => format!(
+            include_str!("nginx.conf"),
+            from = from,
+            to = to,
+            login_group = ""
+        ),
+    }
 }
 
 fn acme_redirect_config_build(namespace: &str) -> String {
-    format!(
-        "[cert]
-name = \"{namespace}\"
-dns_names = [
-    \"{namespace}\",
-]
-exec = [
-    \"systemctl reload nginx\",
-]
-"
-    )
+    format!(include_str!("acme-redirect.conf"), namespace = namespace)
 }
 
 fn generate_webserver_configs(config: &Config, timestring: &str) {
@@ -109,10 +143,21 @@ fn generate_webserver_configs(config: &Config, timestring: &str) {
 
     create_dir_all(&nginx_folder).unwrap();
 
-    for (source, target) in &config.map {
+    for (source, target) in &config.routes {
+        let access_str = if config.login_groups.contains_key(source) {
+            Some(
+                Path::new(&config.home)
+                    .join(config.login_groups.get(source).unwrap())
+                    .to_str()
+                    .unwrap()
+                    .to_string(),
+            )
+        } else {
+            None
+        };
         fs::write(
             nginx_folder.join(format!("{}.nginx", source)),
-            nginx_proxy_build(&source, &target),
+            nginx_proxy_build(&source, &target, access_str),
         )
         .unwrap();
     }
@@ -154,7 +199,7 @@ fn generate_acme_redirect_config(config: &Config, timestring: &str) {
         .unwrap();
     }
 
-    for (namespace, _) in &config.map {
+    for (namespace, _) in &config.routes {
         fs::write(
             acme_path.join(format!("web-distributor.{namespace}.conf")),
             acme_redirect_config_build(&namespace),
@@ -163,14 +208,204 @@ fn generate_acme_redirect_config(config: &Config, timestring: &str) {
     }
 }
 
+fn write_config(config: &Config, config_path: &Path) {
+    fs::write(&config_path, toml::to_string(&config).unwrap()).unwrap();
+}
+
+fn write_passwd(file_string: &str, login_group_path: &Path) {
+    fs::write(login_group_path, file_string).unwrap();
+}
+
 fn main() {
-    let binding = std::env::args().nth(1);
-    let config_path: &Path = match &binding {
-        Some(arg) => Path::new(arg),
+    let cli = Cli::parse();
+
+    let config_path: &Path = match &cli.config {
+        Some(config_str) => Path::new(config_str),
         None => Path::new("/etc/web-distributor.toml"),
     };
 
-    let config = read_config(&config_path);
+    let mut config = read_config(&config_path);
+
+    let login_dir = Path::new(&config.home).join("login_groups");
+    create_dir_all(&login_dir).unwrap();
+
+    let login_groups: Vec<String> = read_dir(&login_dir)
+        .unwrap()
+        .map(|f| f.unwrap().file_name().to_str().unwrap().to_string())
+        .collect();
+
+    for login_group in config.login_groups.values() {
+        if !login_groups.contains(login_group) {
+            write_passwd("", &login_dir.join(login_group));
+        }
+    }
+
+    match &cli.command {
+        Commands::Add {
+            domain,
+            target,
+            force,
+        } => {
+            if config.routes.contains_key(domain) && !force {
+                eprintln!(
+                    "There already exists a route from {} to {}. To override, use --force.",
+                    domain,
+                    config
+                        .routes
+                        .get(domain)
+                        .expect("wtf we just checked if this exists")
+                );
+                exit(1);
+            }
+
+            config.routes.insert(domain.to_string(), target.to_string());
+            write_config(&config, &config_path);
+        }
+        Commands::Remove { domain } => {
+            if !config.routes.contains_key(domain) {
+                eprintln!("Can't remove {domain}, it doesn't exist.");
+                exit(1);
+            }
+            config.routes.remove(domain);
+            write_config(&config, &config_path);
+        }
+        Commands::Generate {} => {}
+        Commands::LoginGroup { pwcommands } => match pwcommands {
+            PasswordCommands::Create { login_group } => {
+                if login_groups.contains(login_group) {
+                    eprintln!("Login group {login_group} already exists.");
+                    exit(1);
+                }
+                write_passwd("", login_dir.join(login_group).as_path());
+                exit(0);
+            }
+            PasswordCommands::Remove { login_group } => {
+                if !login_groups.contains(login_group) {
+                    eprintln!("Login group {login_group} doesn't exist.");
+                    exit(1);
+                }
+                fs::remove_file(&login_dir.join(login_group)).unwrap();
+                config.login_groups.retain(|_, v| v.ne(&login_group));
+                write_config(&config, config_path);
+            }
+            PasswordCommands::Apply {
+                domain,
+                force,
+                login_group,
+            } => {
+                if !login_groups.contains(login_group) {
+                    eprintln!("Login group {login_group} doesn't exist");
+                    exit(1);
+                }
+
+                if !config.routes.contains_key(domain) {
+                    eprintln!("No route with source domain {domain} exists");
+                    exit(0);
+                }
+
+                if config.login_groups.contains_key(domain) && !force {
+                    eprintln!("There already exists an active login group for {domain}. To override, use --force.",);
+                    exit(1);
+                }
+                config
+                    .login_groups
+                    .insert(domain.to_string(), login_group.to_string());
+                write_config(&config, config_path);
+                // here regenerate
+            }
+            PasswordCommands::AddLogin {
+                name,
+                password,
+                force,
+                login_group,
+            } => {
+                if !login_groups.contains(login_group) {
+                    eprintln!("Login group {login_group} doesn't exist");
+                    exit(1);
+                }
+
+                let pwdfile = fs::read_to_string(login_dir.join(login_group)).unwrap();
+                if !force {
+                    for f in pwdfile.lines() {
+                        if f.starts_with(name) {
+                            eprintln!("There already exists a login for {name} in login group {login_group}. To override, use --force.");
+                            exit(0);
+                        }
+                    }
+                }
+                let password = match password {
+                    Some(p) => p.to_string(),
+                    None => rpassword::prompt_password("Enter password:").unwrap(),
+                };
+                let pwdfile = pwdfile
+                    .lines()
+                    .filter(|f| !f.starts_with(name))
+                    .collect::<Vec<&str>>()
+                    .join("\n");
+                let pwdfile = format!(
+                    "{}\n{}",
+                    pwdfile,
+                    format!("{}:{}", name, bcrypt::hash(password, 5).unwrap())
+                );
+                write_passwd(&pwdfile, &login_dir.join(login_group));
+                exit(0);
+            }
+            PasswordCommands::RevokeLogin { name, login_group } => {
+                if !login_groups.contains(login_group) {
+                    eprintln!("Login group {login_group} doesn't exist");
+                    exit(1);
+                }
+
+                let pwdfile = fs::read_to_string(login_dir.join(login_group)).unwrap();
+
+                let mut x = false;
+                for line in pwdfile.lines() {
+                    if line.starts_with(name) {
+                        x = true;
+                        break;
+                    }
+                }
+                if !x {
+                    eprintln!("No login with {name} exists in login group {login_group}");
+                    exit(1);
+                }
+
+                let pwdfile = pwdfile
+                    .lines()
+                    .filter(|f| !f.starts_with(name))
+                    .collect::<Vec<&str>>()
+                    .join("\n");
+                write_passwd(&pwdfile, &login_dir.join(login_group));
+                exit(0);
+            }
+            PasswordCommands::List {} => {
+                for x in login_groups {
+                    println!("{}", x);
+                }
+                exit(0);
+            }
+            PasswordCommands::Disable { domain } => {
+                if !config.login_groups.contains_key(domain) {
+                    eprintln!("No login group is active for {domain}");
+                    exit(1);
+                }
+
+                config.login_groups.remove(domain);
+                write_config(&config, &config_path);
+                // here regenerate
+            }
+        },
+        Commands::List {} => {
+            for (source, destination) in &config.routes {
+                let lg = match config.login_groups.get(source) {
+                    Some(s) => format!(", login group: {}", s),
+                    None => "".to_string(),
+                };
+                println!("{source} => {destination}{}", lg);
+            }
+            exit(0);
+        }
+    }
 
     let timestring = format!(
         "{}",
